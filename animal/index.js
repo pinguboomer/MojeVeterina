579e08b68bbb61367387a0718dc9986b126d2512
Tomdo_ <tomasecekes@gmail.com>
2024-03-02 18:56:01 +0100

    základ Animal examinations service

---
+++ animal/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+/** Express initialization */
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors())
+
+// Logging
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]', { stream: accessLogStream }))
+
+// Routes
+app.use(require('./src/routes/routes'))
+
+/** Mongoose initialization */
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB')
+
+        // Start the Express server
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch(err => console.log(err))
