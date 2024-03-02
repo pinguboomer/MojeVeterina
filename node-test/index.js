c107de39e7cff9a496f3437292fc229c9c663124
Tomdo_ <tomasecekes@gmail.com>
2024-03-02 15:04:06 +0100

    úprava inicializace - spuštění Expressu po připojení k DB

--- node-test/index.js
+++ node-test/index.js
@@ -7,13 +7,15 @@ const app = express()
 // Routes
 app.use(require('./src/routes/routes'))
 
-app.listen(process.env.PORT, () => {
-    console.log(`App is running on port ${process.env.PORT}`)
-})
-
-
 /** Mongoose initialization */
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DB_URI)
-    .then(() => console.log('Connected to MongoDB'))
+    .then(() => {
+        console.log('Connected to MongoDB')
+
+        // Start the Express server
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on port ${process.env.PORT}`)
+        })
+    })
     .catch(err => console.log(err))
