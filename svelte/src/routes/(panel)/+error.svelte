4d1a00957435fe2bb65b79ee1d697a60ca61c9ba
Tomdo_ <tomasecekes@gmail.com>
2024-04-20 20:31:34 +0200

    feat: svelte - hromada změn

--- svelte/src/routes/(panel)/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { page } from '$app/stores';
-    import {Heading, P} from "flowbite-svelte";
-
-    console.log($page)
-</script>
-
-<Heading>{$page.status}</Heading>
-<P>{$page.error?.message}</P>
